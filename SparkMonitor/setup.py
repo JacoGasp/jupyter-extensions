46226b577fba5e2e733a04359e6c4a692b02ebf2
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2021-03-10 14:18:29 +0100

    Updated jupyterlab dependecy for version 2 and 3

--- SparkMonitor/setup.py
+++ SparkMonitor/setup.py
@@ -69,7 +69,7 @@ setup_args = dict(
     cmdclass= cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyterlab~=2.0",
+        "jupyterlab>=2.0",
         "bs4",
     ],
     zip_safe=False,
